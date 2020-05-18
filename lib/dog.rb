efe948134d6df88037a26aea550590bd3993e501
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-18 21:02:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+      puts "Fido"
+  end
+
+  def name
+    @this_dogs_name
+  end
+  
+  def bark=(dog_bark)
+    @this_dogs_bark = dog_bark
+  end  
+  
+  def bark
+    @this_dogs_bark
+      puts "woof!"
+  end
+end
